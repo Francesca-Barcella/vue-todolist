0bdbbff78d3a124d7e2669ccaec161b8f27dae2d
Francesca-Barcella <pollon.fra@gmail>
2022-10-29 14:52:18 +0200

    vt - collegata proprietà text al dom

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,22 +25,56 @@ const { createApp } = Vue
 
 createApp({
     data() {
+
         return {
-            message: 'messaggio di prova',
-            taks: [
+
+            newTask: '',
+
+            error: false,
+            tasks: [
                 {
-                    name: 'Learn HTML',
+                    text: 'learn HTML',
                     done: true
                 },
                 {
-                    name: 'Learn CSS',
+                    text: 'learn CSS',
                     done: true
                 },
                 {
-                    name: 'Learn JS',
+                    text: 'learn JS',
                     done: true
                 },
+                {
+                    text: 'learn VUE JS',
+                    done: true
+                }
             ]
         }
-    }
-}).mount('#app')
+    },
+
+    methods: {
+
+        addTask() {
+            console.log('ho cliccato su add task');
+            //console.log('valore input = ' + this.newTask);
+
+            //devono inserire almeno 5 caratteri
+            if (this.newTask.length < 5) {
+                this.error = true
+            } else {
+                //questi due prima erano fuori e li portiamo dentro l'if-else per risolvere il bug dell'input vuoto
+                this.tasks.unshift(this.newTask)
+                this.newTask = ''
+                this.error = false
+            }
+        },
+
+        done(index) {
+            //fruits.splice(2,1) - toglie dalla posizione 2 un elemento dell'array
+            console.log('ho cliccato sul task con indice N° = ' + index)
+            //tolgo dall'elemento l'elemento attivo che si pesca con il parametro index rilevato al click sullìicona con v-on - 1 indica che tolgie 1 elemento 8se metto due toglie anche l'<li></li> successivo
+            this.tasks.splice(index, 1)
+        }
+    },
+
+}).mount('#app');
